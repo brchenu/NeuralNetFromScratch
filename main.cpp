3b9e462d3e636ac037d4ba87e479fd1152893d2e
brann <branndev@pm.me>
2025-02-02 23:45:05 +0100

    Get magic number from input file

--- main.cpp
+++ main.cpp
@@ -1,17 +1,54 @@
 #include <iostream>
 #include <string>
 #include <fstream>
+#include <vector>
+#include <bitset>
+#include <format>
+#include <sstream>
+#include <cassert>
 
-void parseIdxFile(const std::string &filepath) {
-    std::ifstream file(filepath);
+std::vector<char> parseIdxFile(const std::string &filepath) {
+    std::ifstream file(filepath, std::ios::binary);
 
     if (!file.is_open()) {
         std::cerr << "Error while opening the file";
         std::exit(EXIT_FAILURE);
-    }   
+    }
+
+    file.seekg(0, std::ios::end);
+    std::streamsize length = file.tellg();
+    file.seekg(0, std::ios::beg);
+
+    std::vector<char> buff(static_cast<size_t>(length));
+    file.read(buff.data(), length);
+
+    // Check if we read every expected bytes
+    if (file.gcount() != length) {
+        std::cerr << "Error | File was not read entirely";
+        std::exit(EXIT_FAILURE);
+    }
+
+    file.close();
+
+    return buff;
+}
+
+void printFirstBytes(int size, const std::vector<char> buffer) {
+    assert(size <= buffer.size());
+    for (int i = 0; i < size; i++) {
+        for (int j = 7; j >= 0; j--) {
+            std::cout << ((buffer[i] >> j) & 1);
+        }
+        std::cout << " ";
+    }
+    std::cout << "\n";
 }
 
 int main() {
-    std::cout << "\nStart";
+    std::cout << "Start\n";
+    auto buffer = parseIdxFile("input/train-images.idx3-ubyte");
+
+    printFirstBytes(4, buffer);
+
     return 0;
 }
