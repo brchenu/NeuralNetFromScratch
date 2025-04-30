586cd2e3182a4a80107f3f7653619cafb5d2b100
brann <branndev@pm.me>
2025-04-30 23:25:05 +0200

    Add minibatches and batch training

--- nn_new.py
+++ nn_new.py
@@ -32,6 +32,11 @@ def batch_categorical_cross_entropy(truth, pred):
 		
 	return total_loss
 
+def get_batches(inputs, labels, batch_size):
+	assert(len(inputs) == len(labels))
+	for i in range(0, len(inputs), batch_size):
+		yield inputs[i:i+batch_size], labels[i:i+batch_size]
+
 class MLP():
 	def __init__(self, params: list, activ_func = relu):
 		self.layers = [Layer(param[0], param[1], activ_func) for param in params]
@@ -50,7 +55,12 @@ class MLP():
 			for neuron, grad in zip(layer.neurons, gradients):
 				neuron.backward(grad)
 
-	def stochastic_train(self, inputs, labels):
+	def update(self, learning_rate):
+		for l in self.layers:
+			for neuron in l.neurons:
+				neuron.update(learning_rate)
+
+	def stochastic_train(self, inputs, labels, learning_rate=0.1):
 		'''Stochastic gradient descent training function
 		'''
 		assert(len(inputs) == len(labels)) 
@@ -62,6 +72,8 @@ class MLP():
 
 			# 2. Loss Compute
 			loss = categorical_cross_entropy(label, proba)
+
+			print(f"loss: {loss}")
 		
 		 	# 3. Compute gradients
 			gradients = [y_proba - y_true for y_proba, y_true in zip(proba, label)]
@@ -69,13 +81,58 @@ class MLP():
 			# 4. Propagate gradient back to the network
 			self.backward(gradients)
 
-	# def batch_train(self, inputs, labels):
+			# 5. Update networks parameters
+			self.update(learning_rate)
+		
+	def minibatches_train(self, inputs, labels, batch_size, learning_rate=0.1): 
+		assert(len(inputs) == len(labels))
+		
+		for batch_in, batch_lab in get_batches(inputs, labels, batch_size):
+			total_loss = 0
+			accumulated_gradients = []
 
-	# 	batch_loss = 0.0
-	# 	batch_proba = []
+			for input, label in zip(batch_in, batch_lab):
+				# 1. Forward pass
+				logits = self.forward(input)
+				proba = softmax(logits)
+				
+				# 2. Compute and accumulate loss
+				total_loss += categorical_cross_entropy(label, proba)
 
-	# 	batch_proba = [batch_proba[i] + proba[i] for i in range(proba)]
-	# 	batch_loss += loss 
+				# 3. Compute and accumulate gradients 
+				gradients = [y_proba - y_true for y_proba, y_true in zip(proba, label)]
+				accumulated_gradients.append(gradients)
+			
+			# 4. Average gradients
+			avg_gradients = [sum(grad[i] for grad in accumulated_gradients) / len(batch_in)
+                         for i in range(len(accumulated_gradients[0]))]	
+			
+			self.backward(avg_gradients)
+			self.update(learning_rate)
+
+			print(f"Batch loss: {total_loss / len(batch_in)}")
+
+	def batch_train(self, inputs, labels, learning_rate=0.1):
+		total_loss = 0
+		accumulate_gradient = []
+		for input, label in zip(inputs, labels):
+			# 1. Forward pass
+			logits = self.forward(input)
+			proba = softmax(logits)
+
+			# 2. Compute and accumulate loss
+			total_loss += categorical_cross_entropy(label, proba)
+
+			# 3. Compute and accumulate gradients
+			gradients = [y_proba - y_true for y_proba, y_true in zip(proba, label)]
+			accumulate_gradient.append(gradients)
+
+		avg_gradients = [sum(grad[i] for grad in accumulate_gradient) / len(inputs) for i in range(len(accumulate_gradient[0]))]
+	
+		self.backward(avg_gradients)
+		self.update(learning_rate)
+
+		print(f"Batch loss: {total_loss / len(inputs)}")
 
 class Layer():
 	def __init__(self, nbin: int, nbneurons: int, activ_func):
@@ -112,6 +169,7 @@ class Neuron():
 
 		# Impact of w on L => ∂L/∂w
 		# ∂L/∂w = ∂L/∂z * ∂z/∂w 
+
 		# z = x*w + b so ∂z/∂w = x
 		self.grad_w = [grad_z * x for x in self.inputs]
 		
@@ -125,11 +183,11 @@ class Neuron():
 
 		return grad_x
 	
-	def update(self):
+	def update(self, learning_rate):
 		for grad, w in zip(self.grad_w, self.weights):
-			w -= grad * self.learning_rate
+			w -= grad * learning_rate
 		
-		self.bias -= self.grad_b * self.learning_rate
+		self.bias -= self.grad_b * learning_rate
 			
 mlp = MLP([[3, 3], [3, 2]])
 print(mlp.forward([1, 2, 3]))
