8f088cde4516bd17891f8fe8454cb2b75a3d32bb
brann <branndev@pm.me>
2025-05-22 02:00:57 +0200

    Simplify main.py and clean dataset.py

--- dataset.py
+++ dataset.py
@@ -9,17 +9,18 @@ FILES = [
     "t10k-images-idx3-ubyte.gz",
     "t10k-labels-idx1-ubyte.gz"
 ]
+MAGIC_NUMBER_IMG = 0x00000803
+MAGIC_NUMBER_LABEL = 0x00000801
 
 class MNIST():
     def __init__(self): 
         self.test_images = None
         self.test_labels = None
-        self.images = None
-        self.labels = None
+        self.train_images = None
+        self.train_labels = None
         self.dataset_path = None
     
     def _download_mnist(self):
-
         self.dataset_path = Path("dataset")
         self.dataset_path.mkdir(exist_ok=True)
 
@@ -38,10 +39,11 @@ class MNIST():
 
         print(f"MNIST dataset downloaded !")
 
+    @staticmethod
     def _parse_images(filename):
         with open(filename, 'rb') as f:
             magic, num_images, rows, cols = struct.unpack('>IIII', f.read(16))
-            assert magic == 0x00000803, f"Invalid images magic number"  # 0x00000803 = Image file
+            assert magic == MAGIC_NUMBER_IMG, f"Invalid images magic number"
 
             image_size = rows * cols
             images = f.read(num_images * image_size)
@@ -49,19 +51,55 @@ class MNIST():
 
             return [images[i:i+image_size] for i in range(0, len(images), image_size)]
         
-    def parse_labels(filename):
+    @staticmethod
+    def _parse_labels(filename):
         with open(filename, 'rb') as f:
             magic, num_labels = struct.unpack('>II', f.read(8))
 
-            assert magic == 0x00000801, f"Invalid labels magic number"  # 0x00000801 = Label file
+            assert magic == MAGIC_NUMBER_LABEL, f"Invalid labels magic number"
             labels = f.read(num_labels)
             return list(labels)
+        
+    @property
+    def train_data(self):
+        return self.train_images, self.train_labels
+    
+    @property
+    def test_data(self):
+        return self.test_images, self.test_labels
 
+    def get_train_subset(self, start, end):
+        return self.train_images[start:end], self.train_labels[start:end]
+
+    def get_test_subset(self, start, end):
+        return self.test_images[start:end], self.test_labels[start:end]
+
+    @staticmethod
+    def print_normalized_image(image):
+        density_charset = "@%#*+=-:. "
+        line = ""
+        for i, pixel in enumerate(image):
+            idx = int((len(density_charset) - 1) * pixel)
+            line += density_charset[idx] 
+
+            if (i + 1) % 28 == 0:
+                print(line)
+                line = ""
+    
     def load(self):
         self._download_mnist()
-        self.images = MNIST._parse_images(self.dataset_path / FILES[0].replace('.gz', ''))
-        self.labels = MNIST.parse_labels(self.dataset_path / FILES[1].replace('.gz', ''))
+        self.train_images = MNIST._parse_images(self.dataset_path / FILES[0].replace('.gz', ''))
+        self.train_labels = MNIST._parse_labels(self.dataset_path / FILES[1].replace('.gz', ''))
         self.test_images = MNIST._parse_images(self.dataset_path / FILES[2].replace('.gz', ''))
-        self.test_labels = MNIST.parse_labels(self.dataset_path / FILES[3].replace('.gz', ''))
+        self.test_labels = MNIST._parse_labels(self.dataset_path / FILES[3].replace('.gz', ''))
+
+        # Normalize images
+        self.train_images = [[pixel / 255.0 for pixel in img] for img in self.train_images]
+        self.test_images = [[pixel / 255.0 for pixel in img] for img in self.test_images]
+
+        # Convert labels to one hot encoding vector
+        nb_cls = 10
+        self.train_labels = [[1.0 if i == label else 0.0 for i in range(nb_cls)] for label in self.train_labels]
+        self.test_labels = [[1.0 if i == label else 0.0 for i in range(nb_cls)] for label in self.test_labels]
 
         print(f"MNIST dataset ready !")
--- main.py
+++ main.py
@@ -1,39 +1,8 @@
 from mlp import MLP, cross_entropy_loss
 from dataset import MNIST
 
-def getChar(value):
-    density_line = "@%#*+=-:. "
-    char_idx = (len(density_line) - 1) * value // 255; 
-    return density_line[char_idx]
-
-def print_image(image):
-    line = ""
-    for i, val in enumerate(image):
-        line += getChar(val) 
-        if (i + 1) % 28 == 0:
-            print(line)
-            line = ""
-
-def to_list(label):
-    label_lst = [0.0] * 10
-    label_lst[label] = 1.0
-    return label_lst
-
-def to_one_hot_encoding(labels: list):
-    return [to_list(label) for label in labels]
-
 mnist = MNIST()
 mnist.load()
-
-labels = to_one_hot_encoding(mnist.labels)
-test_labels = to_one_hot_encoding(mnist.test_labels)
-
-# Normalize inputs
-images = [[pixel / 255.0 for pixel in img] for img in mnist.images]
-test_images = [[pixel / 255.0 for pixel in img] for img in mnist.test_images]
-
-sub_img = images[:20000]
-sub_label = labels[:20000]
 
 shape = [784, 128, 10]
 activations = ['relu', 'linear']
@@ -44,9 +13,9 @@ learning_rate = 0.01
 
 mlp = MLP(shape, activations, cross_entropy_loss)
 
-mlp.train(sub_img, sub_label, epochs, batch_size, learning_rate)
+train_img, train_lbl = mnist.get_train_subset(0, 10_000)
 
-sub_test_img = test_images[:1000]
-sub_test_label = test_labels[:1000]
+mlp.train(train_img, train_lbl, epochs, batch_size, learning_rate)
 
-print(f"success rate: {mlp.evaluate(sub_test_img, sub_test_label)}") 
+test_img, test_lbl = mnist.get_test_subset(0, 1000)
+print(f"success rate: {mlp.evaluate(test_img, test_lbl)}") 
