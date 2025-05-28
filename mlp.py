e4ef53d4902532b77e5757a5d9a9ed93ea250f8c
brann <branndev@pm.me>
2025-05-28 19:39:20 +0200

    Import and use utils.py in mlp.py

--- mlp.py
+++ mlp.py
@@ -1,28 +1,8 @@
 import random, math, sys
-
-def relu(x):
-	return max(0, x)
-
-def relu_derivative(x):
-	return 1.0 if x > 0 else 0.0
-
-def linear(x):
-    return x
-
-def linear_derivative(x):
-    return 1
-
-def sigmoid(x):
-    """Sigmoid activation function"""
-    return 1.0 / (1.0 + math.exp(-x)) if x > -700 else 0.0  # Prevent overflow
-
-def sigmoid_derivative(x):
-    """Derivative of sigmoid function"""
-    sigmoid_x = sigmoid(x)
-    return sigmoid_x * (1 - sigmoid_x)
+from utils import relu, relu_derivative, linear, linear_derivative, sigmoid, sigmoid_derivative
 
 def softmax(logits):
-	# Here we need to avoid overflow, if logit is large exp(logit) will be to large
+	# Here we need to avoid overflow, if logit is large exp(logit) will be too large
 	# We are only concerned with relativbe difference between logits
 	# So we find the biggest logit and substract it to every other
 	max_logit = max(logits)
